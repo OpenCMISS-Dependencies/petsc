4477ce5a02488c03deb2fda0c4cc85e02863594b
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-08-20 18:54:56 +0200

    test cases are automatically generated & included if BUILD_TESTS is set to ON. most tests run (exit code 0), but some fail on my machine!

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -1,38 +1,40 @@
 enable_testing()
-set(TESTRUNNER ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TestRunner.cmake)
-macro(ADDTEST NAME NPROC TARGET ARGS)
-    add_test(NAME ${NAME}
-     COMMAND ${CMAKE_COMMAND} 
-    -DMPIEXEC=${MPIEXEC}
-    #-DMPIEXEC=$<TARGET_FILE:helper>
-    -DNPFLAG:STRING=${MPIEXEC_NUMPROC_FLAG}
-    -DNPROC=${NPROC}
-    -DEXEC=$<TARGET_FILE:${TARGET}>
-    -DARGS:STRING=${ARGS}
-    -DOUT=${NAME}.tmp
-    -DWD=${CMAKE_CURRENT_BINARY_DIR}
-    -P ${TESTRUNNER}
-    )
-endmacro()
+set(GENERATED_TESTS_INCFILE ${CMAKE_CURRENT_BINARY_DIR}/IncludeTestFolders.cmake)
 
-add_subdirectory(src/vec/vec/examples/tests)
-add_subdirectory(src/vec/is/is/examples/tests)
-add_subdirectory(src/vec/is/examples/tests)
-add_subdirectory(src/vec/is/ao/examples/tests)
-add_subdirectory(src/ksp/ksp/examples/tests)
-add_subdirectory(src/ksp/pc/examples/tests)
-add_subdirectory(src/snes/examples/tests)
-add_subdirectory(src/mat/examples/tests)
-add_subdirectory(src/tao/unconstrained/examples/tests)
-add_subdirectory(src/dm/impls/plex/examples/tests)
-add_subdirectory(src/dm/impls/moab/examples/tests)
-add_subdirectory(src/dm/impls/patch/examples/tests)
-add_subdirectory(src/dm/dt/examples/tests)
-add_subdirectory(src/dm/examples/tests)
-add_subdirectory(src/sys/error/examples/tests)
-add_subdirectory(src/sys/examples/tests)
-add_subdirectory(src/sys/classes/viewer/examples/tests)
-add_subdirectory(src/sys/classes/draw/examples/tests)
-add_subdirectory(src/ts/examples/tests)
-
-
+if (NOT EXISTS "${GENERATED_TESTS_INCFILE}")
+    find_package(PythonInterp QUIET)
+    if (PYTHONINTERP_FOUND)
+        message(STATUS "Running python test case generator...")
+        execute_process(COMMAND ${PYTHON_EXECUTABLE} 
+            ${CMAKE_CURRENT_SOURCE_DIR}/cmake/convert_tests.py ${CMAKE_CURRENT_BINARY_DIR}
+            RESULT_VARIABLE RES
+            ERROR_FILE ${CMAKE_CURRENT_BINARY_DIR}/conversion.error
+            OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/conversion.out
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+         if (NOT RES EQUAL 0)
+             message(WARNING "Python script for test case generation failed. See ${CMAKE_CURRENT_BINARY_DIR}/conversion.error for details.")
+         endif()
+    else()
+        message(WARNING "No Python interpreter found. Cannot automatically generate test case scripts. No tests will be available.")
+    endif()
+endif()
+            
+if (EXISTS "${GENERATED_TESTS_INCFILE}")
+    set(TESTRUNNER ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TestRunner.cmake)
+    macro(ADDTEST NAME NPROC TARGET ARGS)
+        add_test(NAME ${NAME}
+         COMMAND ${CMAKE_COMMAND} 
+        -DMPIEXEC=${MPIEXEC}
+        #-DMPIEXEC=$<TARGET_FILE:helper>
+        -DNPFLAG:STRING=${MPIEXEC_NUMPROC_FLAG}
+        -DNPROC=${NPROC}
+        -DEXEC=$<TARGET_FILE:${TARGET}>
+        -DARGS:STRING=${ARGS}
+        -DOUT=${NAME}.tmp
+        -DWD=${CMAKE_CURRENT_BINARY_DIR}
+        -P ${TESTRUNNER}
+        )
+    endmacro()
+    
+    include("${GENERATED_TESTS_INCFILE}")
+endif()
