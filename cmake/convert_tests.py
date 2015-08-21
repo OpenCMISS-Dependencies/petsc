d13d4358fdb5f85e815829abd0511117c0c01d33
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-08-21 17:16:58 +0200

    intermediate commit - working on improved test generation
    weekend!

--- cmake/convert_tests.py
+++ cmake/convert_tests.py
@@ -6,26 +6,41 @@ import re
 import sys
 from collections import OrderedDict
  
+global rootDir, numtotaltests, log, debug, binaries, current_folder, missing_sources, missing_execs, alltests
+
 # Set the directory you want to start from
 rootDir = os.getcwd()
 datafiles = os.path.join(rootDir, "share", "petsc", "datafiles") 
  
 # Expressions
+# Finds the whole text for a test
+re_findtest = re.compile(r"^(runex[^:]*):\s*\n((?:(?:.|\n)(?!^runex|TESTEXAMPLES))*)", re.MULTILINE)
+# Finds the test case names in the TESTEXAMPLES_XX lists
+re_testcases = re.compile(r"(runex(?:[^ ]*))", re.MULTILINE)
+# Finds each actual test call
+re_tests = re.compile(r"(?:-@)?\$\{MPIEXEC\} -n (\d+) ./ex(?P<nr>\d+f?) ([^>]*)>>? ex(?P=nr)_?((?:\w+)?)\.tmp 2>&1", re.MULTILINE)
+
 re_binaries = re.compile(r"ex(?P<nr>\d+f?):\s+ex(?P=nr).o\s*chkopts", re.MULTILINE)
-re_tests = re.compile(r"(?:-@)?\$\{MPIEXEC\} -n (\d+) ./ex(?P<nr>\d+f?) ([^>]*)>>? ex(?P=nr)_?((?:\d+)?)\.tmp 2>&1", re.MULTILINE)
-re_getnumbers = re.compile(r"ex(\d+f?).PETSc", re.MULTILINE)
+
+re_testnames = re.compile(r"^(runex(?:([^_f:]*)[^:]*)):", re.MULTILINE)
+# re_getnumbers = re.compile(r"ex(\d+f?).PETSc", re.MULTILINE)
+
 re_vars = re.compile(r"^([A-Z]*)[ \t]*=[ \t]*(.*)\n", re.MULTILINE)
 re_loops = re.compile(r"for (\w+) in \$\{(\w+)\}; do", re.MULTILINE)
 re_loops_direct = re.compile(r"for (\w+) in ([^$][^;]*); do", re.MULTILINE)
 
-#re_test = re.compile(r"(?:-@)?\$\{MPIEXEC\} -n (\d+) ./ex(?P<nr>\d+f?) ([^>]*)>>? ex(?P=nr)_?((?:\d+)?)\.tmp 2>&1", re.MULTILINE)
-#print re_loops_direct.search("""""")
-#print re_tests.findall("""""")
-#sys.exit(0)
+
+# re_test = re.compile(r"(?:-@)?\$\{MPIEXEC\} -n (\d+) ./ex(?P<nr>\d+f?) ([^>]*)>>? ex(?P=nr)_?((?:\d+)?)\.tmp 2>&1", re.MULTILINE)
+# print re_loops_direct.search("""""")
+# m = re_findtest.findall("""""")
+# for ma in m:
+#    print ma
+# print len(m)
+# sys.exit(0)
 
 # This is the type of tests that should be extracted - just trying to build everything would require
 # every possible 3rd-party software to be plugged-in!
-find_examples_types = ["C", "C_X", "FORTRAN", "SUITESPARSE", "MUMPS",
+find_examples_types = ["C", "C_X", "FORTRAN", "SUITESPARSE", "MUMPS", "SUPERLU_DIST",
                        "PASTIX", "PARMETIS", "SUPERLU", "PTSCOTCH", "F2003", "F90", "DATAFILESPATH"]
  
 def getExeName(base, nr):
@@ -45,13 +60,81 @@ def processArgs(args, loopvars):
     # Insert the correct data path
     args = args.replace("${DATAFILESPATH}", datafiles)
     
+    # Stubbornly have the tests use the 32 bit variants
+    args = args.replace("${PETSC_INDEX_SIZE}", "32")
+    args = args.replace("${PETSC_SCALAR_SIZE}", "64")
+    args = args.replace("${PETSC_DIR}", rootDir)
+    
     return args
+
+def writeExecutableCMake(name, exname):
+    ext = 'F' if name.endswith('f') else 'c'
+    src = "ex{}.{}".format(name, ext)
+    fullsrc = os.path.join(current_folder, src)
+    if os.path.isfile(fullsrc):
+        cml.write("add_executable({} {})\n".format(exname, src))
+        cml.write("target_link_libraries({} petsc)\n".format(exname))
+        binaries.append(exname)
+        return 1
+    else:
+        log.write("Skipping binary {}, source {} not found.\n".format(exname, src))
+        missing_sources.append(fullsrc)
+        return 0
+
+def parseTests(base,teststr,vars,forvars):
+    # Write test list
+    #print teststr
+    tests = re_tests.findall(teststr)
+    for match in tests:
+        localcount = 1
+        
+        nproc = match[0]
+        exname = getExeName(base, match[1])
+        if not exname in binaries:
+            created = writeExecutableCMake(match[1], exname)
+            if not created:
+                missing_execs += 1
+                continue
+        
+        testname = getTestName(base, match[1], nproc, match[3])
+        while testname in alltests:
+            testname = (getTestName(base, match[1], nproc, match[3]) + "_v{}").format(localcount)
+            localcount += 1
+        
+        args = processArgs(match[2], forvars);
+        
+        # Check if we have a loop for variables!
+        close = 0
+        testnameloopextra = ''
+        for forvar in forvars:
+            if re.search("\$\$\{?" + forvar, args):
+                loopvals = vars[forvars[forvar]]
+                cml.write("foreach({} {})\n".format(forvar, loopvals))
+                # Replace loop variables by CMake-style variables
+                args = args.replace("$$" + forvar, "${" + forvar + "}")  # case for $$var
+                args = args.replace("$${" + forvar + "}", "${" + forvar + "}")  # case for $${var} - if the first match did not work, those are the cases
+                close += 1
+                testnameloopextra += "_" + forvar + "-${" + forvar + "}"
+        
+        cml.write("ADDTEST({}{} {} {} \"{}\")\n".format(testname, testnameloopextra, nproc, exname, args))
+        alltests.append(testname)
+        if not debug:
+            log.write("Added test {}. nProcs={}, args={}\n"
+                      .format(testname, nproc, args))
+        
+        # Close loops if any!    
+        for num in range(close):
+            cml.write("endforeach()\n")
+        #log.write("Binary {} has not been created, skipping test {}\n".format(exname, testname))
 
 debug = 0
 #debug = 1
+
 folders_with_tests = []
-allexecs = []
+donefolders = []
+binaries = []
 alltests = []
+
 missing_execs = 0
 notmarked = 0
 missing_sources = []
@@ -67,24 +150,24 @@ else:
 for dirName, subdirList, fileList in os.walk(rootDir):
     m = re.match(r"(.*)src/(.*)/examples/(tests|tutorials)", dirName)
     if m:
-        # Get relative folder to working directory
-        folder = m.group(0).replace(m.group(1), "")
+        # Get relative current_folder to working directory
+        current_folder = m.group(0).replace(m.group(1), "")
         
-        if debug and m.group(0) != "/home/dwirtz/software/opencmiss/src/dependencies/petsc/src/mat/examples/tests":
+        if debug and m.group(0) != "/home/dwirtz/software/opencmiss/src/dependencies/petsc/src/ksp/ksp/examples/tutorials":
             continue
         
-        if folder not in folders_with_tests:
+        if current_folder not in donefolders:
             makefile = os.path.join(m.group(0), "makefile")
             
             if not os.path.isfile(makefile):
-                log.write("Skipping folder {}, no 'makefile' found".format(m.group(0)))
+                log.write("Skipping current_folder {}, no 'makefile' found".format(m.group(0)))
                 continue
          
             if debug:
                 cml = sys.stdout
             else:
                 # cml = open(os.devnull, 'w')
-                cml = open(os.path.join(folder, "CMakeLists.txt"), 'w')
+                cml = open(os.path.join(current_folder, "CMakeLists.txt"), 'w')
             
             base = m.group(2).replace("/", "_") + "_" + m.group(3)
             
@@ -99,10 +182,10 @@ for dirName, subdirList, fileList in os.walk(rootDir):
             vars = dict()
             for match in variter:
                 if match.group(2):
-                    #vars[match.group(1)] = filter(None,match.group(2).split(" "))
+                    # vars[match.group(1)] = filter(None,match.group(2).split(" "))
                     vars[match.group(1)] = match.group(2)
             
-            #print vars
+            # print vars
             # Read loop variables
             forvars = dict()
             variter = re_loops.finditer(mfcont)
@@ -113,89 +196,33 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                 tmp_varname = "DIRECTLY_SPECIFIED_{}".format(pos)
                 vars[tmp_varname] = varall[pos][1] 
                 forvars[varall[pos][0]] = tmp_varname 
-            log.write("Found variables: {}\nFound loop variables: {}\n".format(vars,forvars))
+            log.write("Found variables: {}\nFound loop variables: {}\n".format(vars, forvars))
             
-            
-            # Sort out which tests/binaries to actually create
+            # Sort out which tests to actually create
             # This is determined by the TESTEXAMPLES_XX variables at the bottom of every makefile.
             # We only convert the examples for "find_examples_types" (see above)
-            selected_test_main_numbers = []
+            selected_tests = []
             for type in find_examples_types:
                 # Eclipse: TESTEXAMPLES_[^=]*=(.*\\\R)*.*\R
-                expr = ".*TESTEXAMPLES_" + type + "\s*=\s*((?:.*\\\\\n)*)(.*)\n"
+                expr = "^TESTEXAMPLES_" + type + "\s*=\s*((?:.*\\\\\n)*)(.*)\n"
                 match = re.findall(expr, mfcont, re.MULTILINE)
                 if match:
-                    selected_test_main_numbers.extend(re_getnumbers.findall(match[0][0] + match[0][1]))
+                    selected_tests.extend(re_testcases.findall(match[0][0] + match[0][1]))
             
-            # print selected_test_main_numbers
-            # Write executable list
-            testbins = re_binaries.findall(mfcont)
-            for match in testbins:
-                # print "match (nr) = "+match
-                if match in selected_test_main_numbers:
-                    ext = 'F' if match.endswith('f') else 'c'
-                    exname = getExeName(base, match)
-                    src = "ex{}.{}".format(match, ext)
-                    fullsrc = os.path.join(folder, src)
-                    if os.path.isfile(fullsrc):
-                        cml.write("add_executable({} {})\n".format(exname, src))
-                        cml.write("target_link_libraries({} petsc)\n".format(exname))
-                        allexecs.append(exname)
+            if len(selected_tests) > 0:
+                log.write("Test selection: {}".format(selected_tests))
+                folders_with_tests.append(current_folder)
+                # Find tests
+                tests_raw = re_findtest.finditer(mfcont)
+                for test_raw in tests_raw:
+                    if test_raw.group(1) in selected_tests:
+                        log.write("Parsing test {}\n".format(test_raw.group(1)))
+                        parseTests(base,test_raw.group(2),vars,forvars)
                     else:
-                        log.write("Skipping binary {}, source {} not found.\n".format(exname, src))
-                        missing_sources.append(fullsrc)
-                else:
-                    log.write("Skipping binary/test {}, not listed in TESTEXAMPLES section.\n".format(match))
-                    notmarked += 1
-            
-            # Write test list
-            tests = re_tests.findall(mfcont)
-            for match in tests:
-                localcount = 1
-                if match[1] in selected_test_main_numbers:
-                    nproc = match[0]
-                    exname = getExeName(base, match[1])
-                    testname = getTestName(base, match[1], nproc, match[3])
-                    while testname in alltests:
-                        testname = (getTestName(base, match[1], nproc, match[3]) + "_v{}").format(localcount)
-                        localcount += 1
-                    if exname in allexecs:
-                        args = processArgs(match[2],forvars);
-                        
-                        # Check if we have a loop for variables!
-                        close = 0
-                        testnameloopextra = ''
-                        for forvar in forvars:
-                            if re.search("\$\$\{?"+forvar, args):
-                                loopvals = vars[forvars[forvar]]
-                                cml.write("foreach({} in {})\n".format(forvar, loopvals))
-                                # Replace loop variables by CMake-style variables
-                                args = args.replace("$$"+forvar,"${"+forvar+"}") # case for $$var
-                                args = args.replace("$${"+forvar+"}","${"+forvar+"}") # case for $${var} - if the first match did not work, those are the cases
-                                close += 1
-                                testnameloopextra += "_"+forvar+"-${"+forvar+"}"
-                        
-                        cml.write("ADDTEST({}{} {} {} \"{}\")\n".format(testname, testnameloopextra, nproc, exname, args))
-                        alltests.append(testname)
-                        if not debug:
-                            log.write("Added test {}. nProcs={}, args={}\n"
-                                      .format(testname, nproc, args))
-                        
-                        # Close loops if any!    
-                        for num in range(close):
-                            cml.write("endforeach()\n")
-                            
-                    else:
-                        missing_execs += 1
-                        log.write("Binary {} has not been created, skipping test {}\n".format(exname, testname))
-    
-            if len(alltests) > 0:
-                folders_with_tests.append(folder)
-                
+                        log.write("Skipping test {}\n".format(test_raw.group(1)))
             if not debug:
                 cml.close()
-            
-            # sys.exit(0)
+        donefolders.append(current_folder)
         
 # Create an inclusion file that only includes those subfolders that actually have tests created.
 if debug:
@@ -215,7 +242,7 @@ Conversion done.
 Created {} executables and {} tests.
 Missing: {} executables for detected tests, {} source files
 Skipped compilation of {} executables not listed in {}
-""".format(len(allexecs), len(alltests), missing_execs,
+""".format(len(binaries), len(alltests), missing_execs,
             len(missing_sources), notmarked, ",".join(find_examples_types))
 log.writelines(summary)        
 log.close()
